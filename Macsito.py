e76fdaafa994ad1b200ed0042e7602daae197b2e
Max Carrasco <max.carrascoc@duocuc.cl>
2024-06-23 13:55:57 -0400

    PR nuevo

---
+++ Macsito.py
@@ -0,0 +1,19 @@
+# Hola Roger, qué tal?
+# Caché que estay viendo Python, le estay poniendo caleta.
+
+
+# Cuando te metas en lo que son funciones y API avísame, quiero aprender más de eso con Python y estaría weno compartirnos info.
+
+# Cuídese mi chan
+
+
+skull = """
+      ______
+    /      \\
+   |  o  o  |
+   |    ∆    |   < "Arrr!"
+    \  ---  /
+     `-----`
+"""
+
+print(skull)
