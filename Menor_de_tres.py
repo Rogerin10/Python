f74070895da57d71ae64aa6baf6a23b5ac21c1ea
Rogerin <145593459+Rogerin10@users.noreply.github.com>
2025-08-27 20:20:19 -0400

    Add files via upload

---
+++ Menor_de_tres.py
@@ -0,0 +1,13 @@
+print("Ingrese los numeros")
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+num3 = int(input("Ingrese el tercer numero: "))
+
+if num1 < num2 and num1 < num3:
+    menor = num1
+elif num2 < num1 and num2 < num3:
+    menor = num2
+else:
+    menor = num3
+
+print("El numero menor es:", menor)
